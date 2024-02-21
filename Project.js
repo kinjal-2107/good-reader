0bc7f757826025fe75199ee9d6104acc52c729b4
kinjal-2107 <kinjalpatel01791@gmail.com>
2024-02-21 23:08:51 +0530

    first commit for project

---
+++ Project.js
@@ -0,0 +1,5 @@
+const modal = new bootstrap.Modal('#myModal')
+const dropdown = new bootstrap.Dropdown('[data-bs-toggle="dropdown"]')
+const offcanvas = bootstrap.Offcanvas.getInstance('#myOffcanvas')
+const alert = bootstrap.Alert.getOrCreateInstance('#myAlert')
+const modal1 = new bootstrap.Modal(myModalEl, configObject) // initialized with no keyboard
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Project title : GoodReader
+
+> [working Demo](Project.html)
+> 
+> [Working Demo](Book.html)"# good-reader" 
